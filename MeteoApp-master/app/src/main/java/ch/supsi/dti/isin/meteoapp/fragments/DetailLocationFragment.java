c4c76c506456d08496fee918ce728ed47a0a0028
Tommaso Agnola <agnolatommaso@gmail.com>
2019-03-22 15:58:03 +0100

    Aggiornato la grafica e le icone

--- MeteoApp-master/app/src/main/java/ch/supsi/dti/isin/meteoapp/fragments/DetailLocationFragment.java
+++ MeteoApp-master/app/src/main/java/ch/supsi/dti/isin/meteoapp/fragments/DetailLocationFragment.java
@@ -1,10 +1,13 @@
 package ch.supsi.dti.isin.meteoapp.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -24,6 +27,9 @@ public class DetailLocationFragment extends Fragment implements VolleyCallback{
 
     private Location mLocation;
     private TextView mIdTextView;
+    private ImageView mIdimageView;
+
+    private TextView citytv, temperaturetv, humiditytv, descriptiontv;
 
     public static DetailLocationFragment newInstance(UUID locationId) {
         Bundle args = new Bundle();
@@ -51,14 +57,52 @@ public class DetailLocationFragment extends Fragment implements VolleyCallback{
 
         mIdTextView = v.findViewById(R.id.id_textView);
         //mIdTextView.setText(mLocation.getId().toString());
+        mIdimageView = v.findViewById(R.id.weatherIcon);
+        citytv=v.findViewById(R.id.citytv);
+        temperaturetv=v.findViewById(R.id.temperaturetv);
+        humiditytv=v.findViewById(R.id.humiditytv);
+        descriptiontv=v.findViewById(R.id.descriptiontv);
 
         Location currentLocation=LocationsHolder.get(getActivity()).getLocation(mLocation.getId());
 
-        mIdTextView.setText(getString(R.string.app_name)+": "+currentLocation.getName()+
-                "\n"+ getString(R.string.temperature)+": "+currentLocation.getTemperature()
-                +"\n"+ getString(R.string.humidity)+": "+currentLocation.getHumity()
-                +"\n"+getString(R.string.description)+": "+currentLocation.getDescription());
+
+        mIdimageView.setImageDrawable(getDrawableById(currentLocation.getWeatherid()));
+
+        citytv.setText(currentLocation.getName());
+        temperaturetv.setText(currentLocation.getTemperature());
+        humiditytv.setText(currentLocation.getHumity());
+        descriptiontv.setText(currentLocation.getDescription());
         return v;
+    }
+
+    private Drawable getDrawableById(int id){
+        Drawable d;
+        switch (id-(id%100)){
+            case 200://thunderstorm
+                d=getResources().getDrawable(R.drawable.thunderstorm);
+                break;
+            case 300://drizzle
+                d= getResources().getDrawable(R.drawable.snowerrain);
+                break;
+            case 500://rain
+                d= getResources().getDrawable(R.drawable.rain);
+                break;
+            case 600://snow
+                d= getResources().getDrawable(R.drawable.snow);
+                break;
+            case 700://mist
+                d= getResources().getDrawable(R.drawable.mist);
+                break;
+            default://clear and clouds
+                if(id==800) {//clear{
+                    d= getResources().getDrawable(R.drawable.clearsky);
+                }
+                else {//clouds
+                    d= getResources().getDrawable(R.drawable.scatteredclouds);
+                }
+                break;
+        }
+        return d;
     }
 
     @Override
@@ -68,10 +112,11 @@ public class DetailLocationFragment extends Fragment implements VolleyCallback{
         LocationsHolder.get(getActivity()).getLocations().get(index).setName(location.getName());
         Location currentLocation=LocationsHolder.get(getActivity()).getLocation(mLocation.getId());
 
-        mIdTextView.setText(getString(R.string.app_name)+": "+currentLocation.getName()+
-                "\n"+ getString(R.string.temperature)+": "+currentLocation.getTemperature()
-                +"\n"+ getString(R.string.humidity)+": "+currentLocation.getHumity()
-                +"\n"+getString(R.string.description)+": "+currentLocation.getDescription());
+
+        citytv.setText(currentLocation.getName());
+        temperaturetv.setText(currentLocation.getTemperature());
+        humiditytv.setText(currentLocation.getHumity());
+        descriptiontv.setText(currentLocation.getDescription());
     }
 }
 
